3826eafe8602657ad64c6344765059d151f24414
Adil-Omar <madilomar1@yahoo.com>
2025-07-10 17:23:34 +0500

    adil

--- routes/discountRoute.js
+++ routes/discountRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addDiscount, getDiscountByProductId, listDiscount, loginAdmin } from "../controllers/productDiscount.js";
+import { addDiscount, getBatchDiscountsByProductIds, getDiscountByProductId, listDiscount, loginAdmin } from "../controllers/productDiscount.js";
 import adminMiddlewere from "../middleware/adminMiddlewere.js";
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.post("/add-discount", addDiscount, adminMiddlewere);
 router.post("/admin-login", loginAdmin);
 router.get("/discounts/:productId", getDiscountByProductId, adminMiddlewere);
 router.get("/list-discounts", listDiscount);
+router.post('/batch-discounts', getBatchDiscountsByProductIds);
 
 export default router;
